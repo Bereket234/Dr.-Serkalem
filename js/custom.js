d9db439eca5cfb1e01f47b71045578e97a80dfe4
Bereket234 <84328067+Bereket234@users.noreply.github.com>
2021-10-07 15:37:43 +0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,101 @@
+const content= document.querySelectorAll('.content');
 
-const readMore = document.querySelector("#readMore");
-const more = document.querySelector("#hidden");
+const maxNumOfChar= 125
+const sections =document.querySelectorAll('section')
+const nav = document.querySelector(".home-nav-container")
+window.addEventListener('scroll', () =>{
+    let current = '';
+    sections.forEach(section =>{
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if(scrollY > (sectionTop-750) && scrollY <(sectionTop + sectionHeight)){
+            if(section.classList.contains('nav-visible')){
+                console.log('heree');
+               nav.classList.add('activate');
+            }
+            else{
+               nav.classList.remove('activate');
+            }
+        }
+    })
+})
 
-readMore.addEventListener('click',expandMore);
+content.forEach((item)=>{
+    if(item.textContent.length < maxNumOfChar){
+        item.nextElementSibling.style.display= "none";
+    }else{
+        let disText= item.textContent.slice(0, maxNumOfChar);
+        let moreText=  item.textContent.slice(maxNumOfChar);
+        item.innerHTML=`${disText}<span
+         class= "dots">...</span><span class="more hide">${moreText}</span>`;
+    }   
+});
+function readMore(btn){
+    let action= btn.parentElement;
 
-function expandMore(e){
-    e.preventDefault();
-    if (more.style.display === "none"){
-        more.style.display = "block"
-        readMore.innerHTML="Read Less";
-    }
-    else{
-        more.style.display = "none"
-        readMore.innerHTML="Read More";
-    }
-
+    action.querySelector(".dots").classList.toggle("hide");
+    action.querySelector(".more").classList.toggle("hide");
+    btn.textContent=="Read More" ? btn.textContent= "Read Less" : btn.textContent= "Read More";
 }
+const contactForm= document.querySelector('.contact')
+
+let name= document.getElementById('name')
+let phone= document.getElementById('phone')
+let email= document.getElementById('email')
+let message= document.getElementById('message')
+
+contactForm.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        console.log("button clicked")
+let data= {
+    name: name.value,
+    email: email.value,
+    phone: phone.value,
+    message: message.value,
+}
+// $('form').on('submit', (e)=>{
+//     e.preventDefault();
+    
+
+    // const name= $('#name').val().trim();
+    // const phone= $('#phone').val().trim();
+    // const email= $('#email').val().trim();
+    // const message= $('#message').val().trim();
+    // const data= {
+    //     name,
+    //     phone,
+    //     email,
+    //     message
+    // }
+    // console.log(data)
+    // $.post('/', data, (err, res)=>{
+    //    if(err){
+    //        console.log(err);
+    //    }else{
+    //        console.log(res.data)
+    //    }
+    // })
+console.log(data);
+    let xhr=new XMLHttpRequest();
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('content-type', 'application/json')
+    xhr.onload = function(){
+        console.log(xhr.responseText);
+        if(xhr.responseText == 'success'){
+            alert('email sent');
+            name.value= ''
+            phone.value= ''
+            email.value=''
+            message.value= ''
+        }else{
+            alert('sorry message not sent!')
+        }
+    }
+    xhr.send(JSON.stringify(data))
+    console.log(data)
+})
+
+
 // to get current year
 function getYear() {
     var currentDate = new Date();
@@ -62,3 +142,8 @@ function myMap() {
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
+
+
+
+
+
